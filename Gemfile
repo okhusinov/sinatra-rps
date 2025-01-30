b5e581e4ae0db5c60bb4c373b23b9321dce2bf70
okhusinov <okhusinov@chicagobooth.edu>
2025-01-30 09:59:32 +0000

    final

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby "3.2.1"
 
 gem "sinatra"
 gem "sinatra-contrib"
+gem "better_errors"
+gem "binding_of_caller"
 
 # Use Puma as the app server
 gem "puma", "~> 5.0"
--- app.rb
+++ app.rb
@@ -1,9 +1,88 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:main)
+end
+
+get("/rock") do
+  die = rand(1..3)
+  if die == 1
+    die = "rock"
+  elsif die == 2
+    die = "paper"
+  else
+    die = "scissors"
+  end
+
+  @outcome = "They played #{die}!"
+
+  if die == "rock"
+    result = "tied"
+  elsif die == "paper"
+    result = "lost"
+  else
+    result = "won"
+  end
+
+  @winner = "We #{result}!"
+  
+  erb(:rock)
+end
+
+get("/paper") do
+  die = rand(1..3)
+  if die == 1
+    die = "rock"
+  elsif die == 2
+    die = "paper"
+  else
+    die = "scissors"
+  end
+
+  @outcome = "They played #{die}!"
+
+  if die == "paper"
+    result = "tied"
+  elsif die == "scissors"
+    result = "lost"
+  else
+    result = "won"
+  end
+
+  @winner = "We #{result}!"
+  
+  erb(:paper)
+end
+
+get("/scissors") do
+  die = rand(1..3)
+  if die == 1
+    die = "rock"
+  elsif die == 2
+    die = "paper"
+  else
+    die = "scissors"
+  end
+
+  @outcome = "They played #{die}!"
+
+  if die == "scissors"
+    result = "tied"
+  elsif die == "rock"
+    result = "lost"
+  else
+    result = "won"
+  end
+
+  @winner = "We #{result}!"
+  
+  erb(:scissors)
 end
